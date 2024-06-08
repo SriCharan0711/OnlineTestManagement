0c58e492799ed93249af91d2034c3f58b82f9dc9
SriCharan0711 <sricharandarna2003@gmail.com>
2024-06-08 10:22:47 +0530

    8th june

---
+++ Common/Models/QuizResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Models
+{
+    public class QuizResult
+    {
+        public string id { get; set; }
+        public string StudentID { get; set; }
+        public string QuizID { get; set; }
+        public string QuizName {  get; set; }
+        public int TotalQuestions { get; set; }
+        public int CorrectAnswers { get; set; }
+        public double Score { get; set; }
+        public DateTime AttemptDate { get; set; }
+        public Dictionary<string, string> SubmittedAnswers { get; set; }
+    }
+}
